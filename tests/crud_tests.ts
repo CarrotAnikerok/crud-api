a4c1cac3eea1c7638ebb4128cbbe631ccc87584f
carrot <viktoriya.korekina@simpleone.ru>
2026-03-22 23:48:49 +0300

    feat: add get by id test

--- tests/crud_tests.ts
+++ tests/crud_tests.ts
@@ -2,6 +2,7 @@ import { test } from 'node:test';
 import { FastifyInstance } from "fastify";
 import assert from 'node:assert';
 import { buildServer } from '../src/buildServer.js';
+import z from 'zod';
 
 
 test('GET api/products get an empty array scenario', async(t) => {
@@ -40,6 +41,39 @@ test('POST api/products post new product scenario', async(t) => {
     assert.strictEqual(response.statusCode, 201);
     assert.partialDeepStrictEqual(parsedResponse, testProduct);
     assert.strictEqual(parsedResponse.hasOwnProperty('id'), true);
+
+    await fastify.close();
+})
+
+
+test('GET api/products/{productId} post and get created record', async(t) => {
+    const fastify:FastifyInstance = await buildServer();
+
+    const testProduct = {
+        "name": "Dream of the red chamber",
+        "description": "Truth becomes fiction when the fiction’s true; Real becomes not-real when the unreal’s real",
+        "price": 1000,
+        "category": "books",
+        "inStock": false,
+    }
+
+    const postResponse = await fastify.inject({
+        method: 'POST',
+        url: 'api/products',
+        payload: testProduct
+    })
+
+    const response = await fastify.inject({
+        method: 'GET',
+        url: `api/products/${JSON.parse(postResponse.payload).id}`
+    })
+
+    const parsedResponse = JSON.parse(response.payload);
+    const resultId = z.uuid().safeParse(parsedResponse.id);
+
+    assert.strictEqual(response.statusCode, 200);
+    assert.partialDeepStrictEqual(parsedResponse, testProduct);
+    assert.strictEqual(resultId.success, true);
 
     await fastify.close();
 })
